40156b40bb2092d3010932d2e71903a39bb0d28d
Brian Gabini <91449132+briangabini@users.noreply.github.com>
2024-12-27 17:16:03 +0800

    feat: add controller and ctx

--- src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
+++ src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
@@ -1,13 +1,23 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring6DiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring6DiApplication.class, args);
+        // In this class, spring performs @ComponentScan on the package where this class is located
+        // We can also use the application context to get the bean from the context
+        ApplicationContext ctx = SpringApplication.run(Spring6DiApplication.class, args);
+
+        MyController controller = ctx.getBean(MyController.class);
+
+        System.out.println("In Main Method");
+
+        System.out.println(controller.sayHello());
     }
 
 }
