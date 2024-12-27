df0edf65299a0d9002defb5106a1abdbf496303d
Brian Gabini <91449132+briangabini@users.noreply.github.com>
2024-12-27 20:33:55 +0800

    feat: try different types of dependency injection without spring

---
+++ src/test/java/guru/springframework/spring6di/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring6di.controllers;
+
+import guru.springframework.spring6di.services.GreetingServiceImpl;
+import io.micrometer.observation.transport.Propagator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetterInjectedControllerTest {
+
+    SetterInjectedController setterInjectedController;
+
+    @BeforeEach
+    void setUp() {
+        setterInjectedController = new SetterInjectedController();
+        setterInjectedController.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void sayHello() {
+        System.out.println(setterInjectedController.sayHello());
+    }
+}
