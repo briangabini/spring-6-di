204ae4ce3b6f1519968fe47ec2bd7eda749aba78
Brian Gabini <91449132+briangabini@users.noreply.github.com>
2024-12-28 16:53:01 +0800

    feat: use default profiles

--- src/test/java/guru/springframework/spring6di/i18n/myi18NControllerTestsEN.java
+++ src/test/java/guru/springframework/spring6di/i18n/myi18NControllerTestsEN.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("EN")
 @SpringBootTest
 public class myi18NControllerTestsEN {
 
